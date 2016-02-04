87f758eb20f65ae200c9bb858746f5fb0050033e
Spyridon Alfredos Desyllas <sadesyllas@gmail.com>
2016-02-04 12:53:56 +0200

    feat(app): implement serve-fallback

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+
+module.exports = (options) => {
+  options = _.assign({
+    root: '.',
+    fallback: 'index.html',
+    recurse: true,
+  }, _.isPlainObject(options) ? options : {});
+  return (req, res, next) => {
+    const reqUrl = url.parse(req.url);
+    let relativeFilePath = reqUrl.pathname.replace(/^\/|\/$/, '');
+    while (!fs.existsSync(path.resolve(options.root, relativeFilePath))) {
+      req.url = `/${relativeFilePath = _.initial(relativeFilePath.split(/\//)).join('/')}/${options.fallback}`
+      + (reqUrl.search || '')
+      + (reqUrl.hash || '');
+      if (!options.recurse)
+        break;
+    }
+    if (_.isFunction(next))
+      return next();
+  };
+};
